56a5b383adec41dab1fc5ad64c881b1fad7b83c3
oguh43 <agenthugo43@gmail.com>
2022-02-08 20:58:19 +0100

    made loading more reliable

--- inject.js
+++ inject.js
@@ -1,17 +1,19 @@
 EdubarUtils.isAdmin = ()=>{return true};
-ETestUtils.getSuperprideleniaTestu = function(testid, doneFunc) {	
-    var superpridelenia = ETestUtils.getCached('superpridelenia_'+testid);
-        
-    if (!superpridelenia) {	    		
-        $.post('/elearning/?cmd=ETestCreator&akcia=getSuperprideleniaTestu',{testids:testid}, function(pdata) {
-            var data = pdata['superpridelenia'];
-            superpridelenia = data?.[testid] ?? [];
-            ETestUtils.setCached('superpridelenia_'+testid, data);
+window.addEventListener("load",()=>{
+    ETestUtils.getSuperprideleniaTestu = function(testid, doneFunc) {	
+        var superpridelenia = ETestUtils.getCached('superpridelenia_'+testid);
             
-            doneFunc(superpridelenia, pdata);
-            
-        },'json');
-    } else {
-        doneFunc(superpridelenia[testid] ? superpridelenia[testid] : [], superpridelenia);
+        if (!superpridelenia) {	    		
+            $.post('/elearning/?cmd=ETestCreator&akcia=getSuperprideleniaTestu',{testids:testid}, function(pdata) {
+                var data = pdata['superpridelenia'];
+                superpridelenia = data?.[testid] ?? [];
+                ETestUtils.setCached('superpridelenia_'+testid, data);
+                
+                doneFunc(superpridelenia, pdata);
+                
+            },'json');
+        } else {
+            doneFunc(superpridelenia[testid] ? superpridelenia[testid] : [], superpridelenia);
+        }
     }
-}
+})
--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 if (!location.href.includes("elearning")){
     location.href = location.href.match(/.+\.org\//) + "elearning"
 }
-console.log("executing")
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
+var s = document.createElement('script')
+s.src = chrome.runtime.getURL('inject.js')
 s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+    this.remove()
+}
+(document.head || document.documentElement).appendChild(s)
