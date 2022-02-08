d9fc087e0c03ef6f12b3042d48af4ec53e8c939f
oguh43 <agenthugo43@gmail.com>
2022-02-08 20:53:33 +0100

    skeet

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-console.log("loaded extention")
-
-var l = setInterval(()=>{
-    if (!location.href.includes("elearning")){return}
-    console.log("executing")
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('i.js');
-    s.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-    clearInterval(l)
-    
-},3000)
+if (!location.href.includes("elearning")){
+    location.href = location.href.match(/.+\.org\//) + "elearning"
+}
+console.log("executing")
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('inject.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
